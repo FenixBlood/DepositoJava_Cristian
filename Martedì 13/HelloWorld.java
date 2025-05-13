1d1e5849cdd8a2cc5bc18866372d58733b741242
FenixBlood <cristian_platania@hotmail.it>
2025-05-13 18:02:10 +0200

    Delete Martedì 13 directory

--- Martedì 13/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-public static void main(String[] args) {
-    // System.out.println() permette di stampare la string all'interno
-    System.out.println("Hello, World");
-}
-}
